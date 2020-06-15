e28164fbd278c888887d4ab33ee8755f7ab33cc3
JHuraibi <fh1328@wayne.edu>
2020-06-15 18:03:46 -0400

    Trimming and rearranging code
    Streamlined components

--- Question_01.py
+++ Question_01.py
@@ -14,7 +14,7 @@ class ScrambledWordGame(EasyFrame):
         # Separate "initialize" for game variables
         self.initialize_game_variables()
         
-        EasyFrame.__init__(self, width=300, height=200, title="Unscramble the Word")
+        EasyFrame.__init__(self, width=400, height=200, title="Unscramble the Word")
         # Label and field for the Score
         self.addLabel(text="Score: ", column=0, row=0)
         self.fieldScore = self.addIntegerField(value=0, state="readonly", column=1, row=0)
@@ -32,7 +32,10 @@ class ScrambledWordGame(EasyFrame):
         self.fieldNumTries = self.addIntegerField(value=0, state="readonly", column=1, row=3)
         
         # Button to Submit Guess
-        self.addButton(text="Check Guess", column=0, row=4, columnspan=2, command=self.process_player_guess)
+        self.addButton(text="Check Guess", column=2, row=4, command=self.process_player_guess)
+        
+        # Button to Start a New Game
+        self.addButton(text="Start New Game", column=1, row=4, command=self.start_new_game)
     
     def initialize_game_variables(self):
         self.word_bank = None
@@ -41,88 +44,81 @@ class ScrambledWordGame(EasyFrame):
         self.guess = None
         self.score = 0
         self.previous_guesses = []
-        self.__process_words_file()
-        self.__shuffle_words()
+        self.process_words_file()
+        self.shuffle_words()
         
-    def __process_words_file(self):
+    def process_words_file(self):
         """Removes commas from text file. Delimits the words by SPACE and saves as List."""
-        file_in_stream = open("words.txt", 'r')  # Open the in-stream file in read mode
-        file_contents = file_in_stream.read()  # Store file contents as single string
-        file_contents = file_contents.replace(',', '')  # Remove the commas (',')
+        file_in_stream = open("words.txt", 'r')                                 # Open the in-stream file in read mode
+        file_contents = file_in_stream.read()                                   # Store file contents as single string
+        file_contents = file_contents.replace(',', '')                          # Remove the commas (',')
     
-        self.word_bank = file_contents.split()  # Save as list of individual words
+        self.word_bank = file_contents.split()                                  # Save as list of individual words
 
-    def __shuffle_words(self):
+    def shuffle_words(self):
         """Randomize the list of words"""
-        random.shuffle(self.word_bank)  # Randomize the order of the words
+        random.shuffle(self.word_bank)                                          # Randomize the order of the words
 
-    def start_game(self):
-        """Picks and scrambles the next secret word (if word_bank is not empty)"""
+    def start_new_game(self):
+        """Sets and scrambles the next secret word (if word_bank is not empty)"""
         if self.games_remaining():
-            self.__pick_secret_word()
-            self.__scramble_word()
-            # Show a prompt asking for guesses
+            self.set_secret_word()
+            self.scramble_word()
         else:
-            print("No Games Remaining")
-            # Show a prompt asking if player wants to play again or exit
+            self.messageBox(title="No Games", message="No Words Remaining!")
 
-    def __pick_secret_word(self):
+    def set_secret_word(self):
         """Pops off the next word from the list"""
-        self.secret_word = self.word_bank.pop()  # Get secret word (and delete from list)
+        self.secret_word = self.word_bank.pop()                                 # Get secret word (and delete from list)
 
-    def __scramble_word(self):
+    def scramble_word(self):
         """Scrambles the characters in the secret word"""
-        word = list(self.secret_word)  # Convert word to List of characters
+        word = list(self.secret_word)                                           # Convert word to List of characters
     
-        random.shuffle(word)  # Randomly shuffle around the letters
-        word = ''.join(word)  # "Convert" back to a single string
+        random.shuffle(word)                                                    # Randomly shuffle around the letters
+        word = ''.join(word)                                                    # "Convert" back to a single string
     
-        self.scrambled_secret_word = word  # Store scrambled version of the word
-
-    def games_remaining(self):
-        """Returns whether there are any available games left (i.e. there are words in word_bank)"""
-        return len(self.word_bank) > 0
+        self.scrambled_secret_word = word                                       # Store scrambled version of the word
 
     def process_player_guess(self):
-        """Gets the player's guess. If guess non-alpha or already tried, reattempt. Then stores the guess"""
-        self.guess = self.__guess_helper()
+        """Gets the player's guess. Alerts if guess non-alpha or already tried.
+        Will record the guess if it was valid"""
 
-    def __guess_helper(self):
-        """Recursive method to get player's guess and check if its valid"""
-        # If time: Determine if functions below are unnecessary
-    
-        guess = input("Enter Your Guess: ")  # Get the player's guess
-    
-        valid_guess = self.__has_letters_only(guess)  # Only letters entered?
-        old_guess = self.__guess_already_tried(guess)  # Guess already tried?
-    
-        if not valid_guess:
-            print("\nInvalid guess. Letters only!")  # Invalid guess (non-alpha)
-            return self.__guess_helper()
+        guess = self.fieldGuess.getText()                                       # Get player's guess from the text field
+
+        no_guess_entered = len(guess) == 0                                      # No guess entered
+        valid_guess = str(guess).isalpha()                                      # Only letters entered?
+        old_guess = self.guess_already_tried(guess)                             # Guess already tried?
+
+        if no_guess_entered:
+            self.messageBox(title="Invalid Guess",
+                            message="No Guess Entered")                         # Nothing was entered
+        elif not valid_guess:
+            self.messageBox(title="Invalid Guess",
+                            message="Guesses can only have letters!")           # Invalid guess entered (non-alpha)
         elif old_guess:
-            print("\n\"{}\" was already tried".format(guess))  # Guess was already tried
-            return self.__guess_helper()
+            self.messageBox(title="Invalid Guess",
+                            message="\"{}\" was already tried".format(guess))   # Guess was already tried
         else:
-            return guess  # (Base Case): Guess is valid
+            self.record_guess(guess)                                            # Guess was good, record it
 
-    @staticmethod
-    def guess_valid(value_entered):
-        return str(value_entered).isalpha()
-
-    def __record_guess(self, most_recent_guess):
-        self.previous_guesses.append(most_recent_guess)
-
-    def __has_letters_only(self, guess):
-        """Returns true if the player's guess only contains letters"""
-        return str(guess).isalpha()
-
-    def __guess_already_tried(self, guess):
+    def guess_already_tried(self, guess):
         """Returns true if the player's guess matches a guess they already attempted"""
-        return guess in self.previous_guesses
+        return guess.lower() in self.previous_guesses
+    
+    def record_guess(self, guess):
+        """Records the most-recent valid guess by converting to lower case and adding to List"""
+        lower_case_guess = guess.lower()
+        self.previous_guesses.append(lower_case_guess)
 
     def guessed_correctly(self, guess):
         """Returns true if the player's guess was correct (matches the secret word)"""
-        return str(self.guess) == self.secret_word
+        return str(guess).lower() == self.secret_word.lower()
+    
+    def games_remaining(self):
+        """Returns whether there are any available games left (i.e. there are words in word_bank)"""
+        return len(self.word_bank) > 0
+    
 
 def main():
     ScrambledWordGame().mainloop()
