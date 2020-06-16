782e5cc41d5b5026e37ec4bc1f7c5f0b36cde3eb
JHuraibi <fh1328@wayne.edu>
2020-06-15 22:02:35 -0400

    Testing Done. (See Notes)
    !! Line 89 needs to be disabled/delete for purposes other than testing
    Notes:
    - Added a check for missing "words.txt" file
    - Changed most pop-up messages to a single label on GUI window
    - Fixed oversight of calling "start_new_game" twice
    - PEP8 format good

--- Question_01.py
+++ Question_01.py
@@ -2,9 +2,6 @@
 # Assignment 03
 # Question 01
 
-# PEP8 CURRENTLY NOT BEING FOLLOWED
-
-
 import random
 from breezypythongui import EasyFrame
 
@@ -19,12 +16,7 @@ class ScrambledWordGame(EasyFrame):
         self.tries_remaining = 3
         self.previous_guesses = []
         
-        self.process_words_file()                                               # Load words from text file
-        self.shuffle_words()                                                    # Randomize the list of words
-        self.set_secret_word()                                                  # Set the first secret word
-        self.scramble_word()                                                    # Scramble the secret word
-        
-        EasyFrame.__init__(self, width=400, height=150, title="Unscramble the Word")
+        EasyFrame.__init__(self, width=400, height=200, title="Unscramble the Word")
         
         # Label and field for the Scrambled Word
         self.addLabel(text="Scrambled Word: ", row=0, column=0)
@@ -35,19 +27,36 @@ class ScrambledWordGame(EasyFrame):
         self.fieldScore = self.addIntegerField(value=0, state="readonly", sticky="W", row=0, column=4)
         
         # Label and field the Player's Guess Input
-        self.addLabel(text="Your Guess: ", row=1, column=0)
+        self.addLabel(text="Your Guess: ", sticky="E", row=1, column=0)
         self.fieldGuess = self.addTextField(text="", row=1, column=1)
 
         # Label and field number of Guesses Remaining
         self.addLabel(text="Tries Remaining: ", row=2, column=0)
         self.fieldTriesRemaining = self.addIntegerField(value=0, state="readonly", sticky="W", row=2, column=1)
+        
+        # Bottom-most label for messages to player
+        self.labelMessage = self.addTextField(text="", state="readonly", sticky="NSEW",
+                                              columnspan=5, row=4, column=0)
 
         # Buttons
-        self.checkGuessButton = self.addButton(text="Check Guess", row=4, column=0, command=self.process_player_guess)
-        self.newGameButton = self.addButton(text="Start New Game", row=4, column=3, command=self.start_new_game)
+        self.checkGuessButton = self.addButton(text="Check Guess", row=3, column=0, command=self.process_player_guess)
+        self.newGameButton = self.addButton(text="New Game", row=3, column=3, command=self.start_new_game)
         
+        self.preliminary_work()                                                 # Execute preliminary tasks
+    
+    def preliminary_work(self):
+        """Checks if text file exists and continues if it does. Otherwise, exits program"""
+        import os
+        text_file_exists = os.path.exists("words.txt")                          # Check for text file of words
+        
+        if not text_file_exists:
+            print("Error opening text file.")
+            exit()
+            
+        self.process_words_file()                                               # Load words from text file
+        self.shuffle_words()                                                    # Randomize the list of words
         self.start_new_game()                                                   # Start the game
-        
+    
     def process_words_file(self):
         """Removes commas from text file. Delimits the words by SPACE and saves as List."""
         file_in_stream = open("words.txt", 'r')                                 # Open the in-stream file in read mode
@@ -56,6 +65,8 @@ class ScrambledWordGame(EasyFrame):
         
         self.word_bank = file_contents.split()                                  # Save as list of individual words
         file_in_stream.close()                                                  # Close the file stream
+        
+        return True
 
     def shuffle_words(self):
         """Randomize the list of words"""
@@ -70,25 +81,24 @@ class ScrambledWordGame(EasyFrame):
         
         self.fieldGuess.setText("")                                             # Clear the guess textbox
         self.fieldScrambledWord.setText(self.scrambled_secret_word)             # Update Scrambled Word
-        self.fieldScore.setValue(self.score)                                    # Update Score
         self.fieldTriesRemaining.setValue(self.tries_remaining)                 # Update (reset) tries remaining
         
-        self.checkGuessButton["state"] = "normal"                               # Enable the GUI buttons
-        self.newGameButton["state"] = "normal"
-        
-        print("WORD: {}".format(self.secret_word))
+        self.checkGuessButton["state"] = "normal"                               # Enable "Check Guess" button
+        self.newGameButton["state"] = "normal"                                  # Ensure "New Game" button is enabled
+        self.labelMessage.setText("")                                           # Clear the bottom message label
+        print("WORD: {}".format(self.secret_word))  # !! FOR TESTING
 
     def games_remaining(self):
-        """Returns whether there are any available games left (i.e. there are words in word_bank)"""
+        """Returns whether there are any available games left (i.e. words remaining in List word_bank)"""
         return len(self.word_bank) > 0
 
     def set_secret_word(self):
-        """Pops off the next word from the list"""
+        """Pops off the next word from the shuffled List"""
         self.secret_word = self.word_bank.pop()                                 # Get secret word (and delete from list)
 
     def scramble_word(self):
         """Scrambles the characters in the secret word.
-        While-Loop will handle off-chance scrambled word is still the same"""
+        Loop will handle off-chance scrambled word is still equals the original."""
         word = self.secret_word
         
         while word == self.secret_word:
@@ -111,27 +121,24 @@ class ScrambledWordGame(EasyFrame):
         old_guess = self.guess_already_tried(guess)                             # Guess already tried?
 
         if no_guess_entered:
-            self.messageBox(title="Invalid Guess",
-                            message="No Guess Entered")                         # Nothing was entered
+            self.labelMessage.setText("No Guess Entered")                       # Nothing was entered
         elif not valid_guess:
-            self.messageBox(title="Invalid Guess",
-                            message="Guesses can only have letters!")           # Invalid guess entered (non-alpha)
+            self.labelMessage.setText("Guesses can only have letters!")         # Invalid guess entered (non-alpha)
         elif old_guess:
-            self.messageBox(title="Invalid Guess",
-                            message="\"{}\" was already tried".format(guess))   # Guess was already tried
+            self.labelMessage.setText("\"{}\" already tried".format(guess))     # Guess was already tried
         else:
             self.guess = guess                                                  # Guess entered was good, store it
             self.check_guess()                                                  # Check guess
             
     def check_guess(self):
         """Skips program to end-of-game procedures if guess was correct"""
-        current_guess = self.guess.lower()
-        secret_word = self.secret_word.lower()
+        current_guess = self.guess.lower()                                      # Convert guess string to lowercase
+        secret_word = self.secret_word.lower()                                  # Ensure secret word is lowercase
         
         if current_guess == secret_word:
-            self.messageBox(title="", message="Congratulation, you won!")       # User guessed word correctly
-            self.update_score()
-            self.end_game()
+            self.update_score()                                                 # Update their score
+            self.messageBox(title="", message="Congratulations, you won!")      # User guessed word correctly
+            self.end_game()                                                     # Execute end-game tasks
         else:
             self.next_round()                                                   # Incorrect, execute end-of-round tasks
 
@@ -148,7 +155,9 @@ class ScrambledWordGame(EasyFrame):
         """Updates the player's score and updates the output on the GUI"""
         points_earned = len(self.secret_word) * 10                              # 10 pts. for each letter of secret word
         self.score = self.score + points_earned                                 # Add to total score
-    
+        
+        self.fieldScore.setValue(self.score)                                    # Update score on GUI
+
     def next_round(self):
         """Handles end of round actions"""
         self.tries_remaining = self.tries_remaining - 1                         # Deduct a try
@@ -157,22 +166,21 @@ class ScrambledWordGame(EasyFrame):
         if self.tries_remaining > 0:
             self.previous_guesses.append(self.guess)                            # Record most-recent guess tried
             self.fieldTriesRemaining.setNumber(self.tries_remaining)            # Update tries user has remaining
-            self.messageBox(title="", message="The guess was incorrect")        # Inform player guess was wrong
+            self.labelMessage.setText("The guess was incorrect.")               # Inform player guess was wrong
         else:
             self.messageBox(title="", message="Sorry, you didn’t win")          # No tries remaining
-            self.end_game()
+            self.end_game()                                                     # Execute end-game tasks
 
     def end_game(self):
-        """Handles end of game"""
+        """Handles end of game tasks"""
         if self.games_remaining():
             self.checkGuessButton["state"] = "disabled"                         # Disable "Check Guess" button
-            self.messageBox(title="",
-                            message="Click \"Start new Game\" to play again")   # Tell user how to start new game
+            self.labelMessage.setText("Click \"New Game\" to continue")         # Tell user how to play again
         else:
-            self.messageBox(title="", message="No Words Remaining!")            # Inform player no games left
+            self.labelMessage.setText("No Words Remaining!")                    # Inform player no games left
             self.checkGuessButton["state"] = "disabled"                         # Disable the buttons
             self.newGameButton["state"] = "disabled"
-    
+
 
 def main():
     """Instantiates the GUI window and starts the game"""
